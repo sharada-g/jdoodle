e982a14518f3073c325434d260b12de8904a541a
sharada-g <gunathilakesharada@gmail.com>
2023-05-01 05:22:12 +1000

    proxy server for cors

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.jdoodle.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/v1')
+      }
+    }
   }
 })
